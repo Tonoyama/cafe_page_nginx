4e573f441878ff0a41f3460a7ab154842e28af8f
Tonoyama <yudaitonoyama@gmail.com>
2021-05-30 17:41:36 +0900

    初期データを挿入

--- myapp/delete.py
+++ myapp/delete.py
@@ -6,3 +6,9 @@ print("全レコード削除")
 db_session.query(SensorCurrent).delete()
 db_session.commit()
 db_session.close()
+
+# 初期データ
+data = SensorCurrent(25,30)
+db_session.add(data)
+db_session.commit()
+db_session.close()
