4cc3c037fc89183846d6ae80dd0a9a764d5416c2
Tonoyama <yudaitonoyama@gmail.com>
2021-05-30 18:26:22 +0900

    DB削除時のコメントを追加

--- myapp/delete.py
+++ myapp/delete.py
@@ -2,13 +2,18 @@ from flask import Flask, render_template, request, json, jsonify
 from models.models import SensorCurrent
 from models.database import db_session
 
-print("全レコード削除")
+print("レコード削除中...")
 db_session.query(SensorCurrent).delete()
 db_session.commit()
 db_session.close()
+
+print("削除完了")
+print("初期データ挿入中...")
 
 # 初期データ
 data = SensorCurrent(25,30)
 db_session.add(data)
 db_session.commit()
 db_session.close()
+
+print("挿入完了")
