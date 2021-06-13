c72e5ff1111a72a4a988d26927d623ea0b83e8c9
Tonoyama <yudaitonoyama@gmail.com>
2021-06-14 03:21:55 +0900

    CSV に保存できるようにした

--- myapp/toridashi.py
+++ myapp/toridashi.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, request, json, jsonify
 from models.models import SensorCurrent
 from models.database import db_session
+import csv
 
 print("実行中...")
 
@@ -10,4 +11,9 @@ for user in users:
     j = user.j_merged_num
     z = user.z_merged_num
     date = user.date
-    print(j, z, date.strftime('%Y-%m-%d %H:%M:%S'))
+    date = date.strftime('%Y-%m-%d %H:%M:%S')
+    print(j, z, date)
+
+with open('output.csv', 'w') as f:
+    writer = csv.writer(f)
+    writer.writerow([j, z, date])
